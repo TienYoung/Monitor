8cd35f66d5e3352eb8ca4782a3b6219cf3495bbb
Tien Young <tien.young@outlook.com>
2025-08-24 13:01:22 -0400

    Updated winui2

--- Kiosk/MainPage.xaml.cs
+++ Kiosk/MainPage.xaml.cs
@@ -1,4 +1,11 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using System.IO.Ports;
 
 namespace Kiosk
 {
@@ -7,9 +14,64 @@ namespace Kiosk
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private CoreApplicationViewTitleBar _coreTitleBar;
+        private ApplicationViewTitleBar _titleBar;
+
         public MainPage()
         {
             InitializeComponent();
+
+            // Set XAML element as a drag region.
+            Window.Current.SetTitleBar(AppTitleBar);
+            // Register a handler for when the window activation changes.
+            Window.Current.CoreWindow.Activated += CoreWindow_Activated;
+
+            // Hide default title bar.
+            _coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            _coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            // Register a handler for when the size of the overlaid caption control changes.
+            _coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            // Register a handler for when the title bar visibility changes.
+            // For example, when the title bar is invoked in full screen mode.
+            _coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+
+            // Set caption buttons background to transparent.
+            _titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            _titleBar.ButtonBackgroundColor = Colors.Transparent;
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            // Get the size of the caption controls and set padding.
+            LeftPaddingColumn.Width = new GridLength(_coreTitleBar.SystemOverlayLeftInset);
+            RightPaddingColumn.Width = new GridLength(_coreTitleBar.SystemOverlayRightInset);
+        }
+
+        private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (sender.IsVisible)
+            {
+                AppTitleBar.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void CoreWindow_Activated(CoreWindow sender, WindowActivatedEventArgs args)
+        {
+            UISettings settings = new UISettings();
+            if (args.WindowActivationState == CoreWindowActivationState.Deactivated)
+            {
+                AppTitleTextBlock.Foreground = new SolidColorBrush(settings.UIElementColor(UIElementType.GrayText));
+            }
+            else
+            {
+                AppTitleTextBlock.Foreground = new SolidColorBrush(settings.UIElementColor(UIElementType.WindowText));
+            }
         }
     }
 }
